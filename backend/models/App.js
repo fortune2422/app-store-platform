fcaa668e4ed37c9c8b07eeec73826a30d81a0214
fortune2422 <fortuntestccount242@gmail.com>
2025-12-07 19:07:36 +0700

    Update App.js

--- backend/models/App.js
+++ backend/models/App.js
@@ -1,12 +1,85 @@
+// backend/models/App.js
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config");
 
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define("App", {
-    name: DataTypes.STRING,
-    packageName: DataTypes.STRING,
-    version: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    iconUrl: DataTypes.STRING,
-    screenshots: { type: DataTypes.JSON, defaultValue: [] },
-    apkUrl: DataTypes.STRING,
-    downloadCount: { type: DataTypes.INTEGER, defaultValue: 0 }
-  });
+const App = sequelize.define("App", {
+  // 基础信息
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  // 例如：GO606-33
+  code: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  packageName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  version: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+
+  // 商店信息
+  developerName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  rating: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+    defaultValue: 4.8
+  },
+  reviewsCount: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0
+  },
+  downloadsLabel: {
+    // 如： "2M+"
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  sizeLabel: {
+    // 如： "25 MB"
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  updatedAtLabel: {
+    // 如："Dec 7, 2025"
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+
+  // 资源
+  iconUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  desktopIconUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  bannerUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  screenshots: {
+    // 存多张截图 URL
+    type: DataTypes.ARRAY(DataTypes.TEXT),
+    allowNull: false,
+    defaultValue: []
+  },
+  apkUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+});
+
+module.exports = { App };
