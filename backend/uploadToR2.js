d638b9be050aa0b16a29adb60d2ef7399f0a6d46
fortune2422 <fortuntestccount242@gmail.com>
2025-12-07 17:25:14 +0700

    Update uploadToR2.js

--- backend/uploadToR2.js
+++ backend/uploadToR2.js
@@ -1,15 +1,22 @@
-
+// backend/uploadToR2.js
 const { PutObjectCommand } = require("@aws-sdk/client-s3");
 const r2 = require("./r2");
 const { v4: uuid } = require("uuid");
 
 module.exports = async function uploadToR2(file, folder) {
   const key = `${folder}/${uuid()}-${file.originalname}`;
-  await r2.send(new PutObjectCommand({
-    Bucket: process.env.R2_BUCKET,
-    Key: key,
-    Body: file.buffer,
-    ContentType: file.mimetype
-  }));
-  return `${process.env.R2_PUBLIC_URL}/${key}`;
+  console.log("uploadToR2 => bucket:", process.env.R2_BUCKET, "key:", key);
+
+  await r2.send(
+    new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET,
+      Key: key,
+      Body: file.buffer,
+      ContentType: file.mimetype
+    })
+  );
+
+  const url = `${process.env.R2_PUBLIC_URL}/${key}`;
+  console.log("uploadToR2 => public url:", url);
+  return url;
 };
