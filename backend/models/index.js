24677670467dc630737c5594bad5b17c1133aa3b
fortune2422 <fortuntestccount242@gmail.com>
2025-12-11 20:04:41 +0700

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,8 @@
 // backend/models/index.js
 const sequelize = require("../sequelize");
 const App = require("./App");
+const Domain = require("./Domain")(sequelize, Sequelize.DataTypes);
+db.Domain = Domain;
 
 async function initDb() {
   try {
